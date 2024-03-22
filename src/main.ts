13fbc64c974642b354ff193c939eb8cb131e3aa4
Cydnirn <seno@siotics.org>
2024-03-22 11:40:50 +0700

    feat: nest init

---
+++ src/main.ts
@@ -0,0 +1,31 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import fastifyHelmet from '@fastify/helmet';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter({ logger: true }),
+  );
+
+  app.register(fastifyHelmet, {
+    contentSecurityPolicy: false,
+  });
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Concert API')
+    .setDescription('Concert API Description')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3000, '0.0.0.0');
+}
+bootstrap();
